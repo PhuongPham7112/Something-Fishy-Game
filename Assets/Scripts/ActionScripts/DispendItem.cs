0f09db41939b065b5567cd42fb438fb491e5122b
PhuongPham7112 <alassycottery@gmail.com>
2023-09-03 15:04:13 -0700

    Add Mr Crab properly

---
+++ Assets/Scripts/ActionScripts/DispendItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispendItem : InteractableFunctionality
+{
+    public override void Action(bool isClicked, string key = "")
+    {
+        base.Action(isClicked, key);
+    }
+
+    public override void ClearEffect()
+    {
+        base.ClearEffect();
+    }
+}
