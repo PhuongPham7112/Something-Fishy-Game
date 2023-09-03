3658371ea7d778c38a40d3f875e2fbfbe379551c
PhuongPham7112 <alassycottery@gmail.com>
2023-09-03 14:49:24 -0700

    Complete water plane

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,10 @@ public class PlayerMovement : MonoBehaviour
     float speed;
     [SerializeField]
     float upDownSpeed;
+    [SerializeField]
+    Transform waterPlane;
 
     private Rigidbody rb;
-    private CapsuleCollider capsuleCollider;
     private float timeCount = 0.0f;
     // Define the maximum and minimum rotation angles
     float maxRotationAngle = -60f;
@@ -22,25 +23,21 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        capsuleCollider = GetComponent<CapsuleCollider>();
-    }
-
-    void Update()
-    {
-
     }
 
     void FixedUpdate()
     {
+        float yDifference = waterPlane.position.y - transform.position.y;
+
         // Moving Forward
-        if (Input.GetKey(KeyCode.Space) && !Physics.Raycast(transform.position, transform.forward, 1f))
+        if (Input.GetKey(KeyCode.Space))
         {
             Vector3 forwardMovement = speed * Time.fixedDeltaTime * - transform.up;
             rb.MovePosition(rb.position + forwardMovement);
         }
 
         // Moving Up
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && yDifference > 0f)
         {
             Vector3 upMovement = Time.fixedDeltaTime * upDownSpeed * Vector3.up;
             rb.MovePosition(rb.position + upMovement);
@@ -82,7 +79,7 @@ public class PlayerMovement : MonoBehaviour
             rb.MoveRotation(Quaternion.Slerp(rb.rotation, rotatedRotation, 5.0f * Time.deltaTime));
         }
         // Turning up
-        else if (Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W) && yDifference > 0f)
         {
             newRotation = Quaternion.AngleAxis(-turnSpeed * Time.fixedDeltaTime, Vector3.right);
             Quaternion rotatedRotation = rb.rotation * newRotation;
